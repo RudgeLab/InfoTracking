1f41b1da6ea25ba40eeb9b32849c680ad625eea9
Ignacio Medina <inmedina@uc.cl>
2018-05-08 00:32:06 -0400

    Done!
    Check for bugs or other stuff, it will show plots for now

--- PickleVelocity.py
+++ PickleVelocity.py
@@ -1,7 +1,7 @@
 import cPickle
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+
 def pos2pixel(ps,factr):
     return factr*ps
 
@@ -30,78 +30,77 @@ def velplotter(image,position1,position2,velocity,j):
     plt.plot(position1[:,0],position1[:,1],"ro",markersize = 0.5)
     plt.quiver(position1[:,0],position1[:,1],velocity[:,0],velocity[:,1])
     #plt.show()
-    fig1.savefig('velstep-00'+str(startframe+j)+'0.pdf')
+    #fig1.savefig('velstep-00'+str(startframe+j)+'0.pdf')
 
 '''-------MAIN-----'''
 plt.ioff()
 
-dt=100
-nframes = 20 
-startframe = 6 
+dt=1
+nframes = 10
+startframe = 50
  
-fname = "/Users/timrudge/cellmodeller/data/weiner-17-12-07-17-56/step-%04d0.pickle"
-#fname = "/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-00%0d0.pickle"
-fname2 = "/Users/timrudge/cellmodeller/data/weiner-17-12-07-17-56/step-%04d0.png"
-#fname2 = "/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-00%0d0.png"
+#fname = "/Users/timrudge/cellmodeller/data/weiner-17-12-07-17-56/step-%04d0.pickle"
+fname = "/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-%04d0.pickle"
+#fname2 = "/Users/timrudge/cellmodeller/data/weiner-17-12-07-17-56/step-%04d0.png"
+fname2 = "/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-%04d0.png"
 
-im1 = np.array([cPickle.load(open(fname%(startframe+i))) for i in range(nframes)])
-im2 = np.array([cPickle.load(open(fname%(startframe+i+1))) for i in range(nframes)])
-'''
-data1 = cPickle.load(open("/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-00520.pickle", 'rb'))
-data2 = cPickle.load(open("/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-00530.pickle", 'rb'))
-'''
-imgs = np.array([mpimg.imread(open(fname2%(startframe+i))) for i in range(nframes)])
-#img = mpimg.imread("/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-00520.png")
+data = np.array([cPickle.load(open(fname%(startframe+i))) for i in range(nframes)])
+imgs = np.array([plt.imread(open(fname2%(startframe+i))) for i in range(nframes)])
 
-csa = np.array([element['cellStates'] for element in im1])
-#csa = im1['cellStates']
-#csb = data2['cellStates']
+
+csa = np.array([element['cellStates'] for element in data])
+
 
 worldsize = 250.0
 imagesize = imgs[0].shape[0]
 
-resizing= imagesize/worldsize #not so good
-C = imagesize/2 #not so good
-#C = 590.5
-#resizing = 4.724
+resizing= imagesize/worldsize 
+C = imagesize/2 
+
 
 print "worldsize = ", worldsize
 print "imagesize = ", imagesize
-print "resizing = ", resizing
+print "resizing factor = ", resizing
 
-#vel,pos1,pos2 = np.array([Pickledx(csa[i],csa[i+1],1,resizing) for i in range(nframes-1)])
-velpos = np.array([Pickledx(csa[i],csa[i+1],dt,resizing) for i in range(nframes-1)])
+
+velpos = np.array([Pickledx(csa[i],csa[i+1],dt,resizing) for i in range(nframes-1)]) #dim (nframes-1,3) with dx,x1,x2
 
 #pos1 = np.array([[-a,b,c] for (a,b,c) in pos1]) #fix axis, after noticing how to fix: a simpler way would be pos[:,0] = -pos[:,0]
-#pos2 = np.array([[-a,b,c] for (a,b,c) in pos2]) 
+
 for element in velpos:
     element[1] = np.array([[C+a,C-b,c] for (a,b,c) in element[1]])
 for item in velpos:
     item[2] = np.array([[C+a,C-b,c] for (a,b,c) in item[2]])
-#pos1 = np.array([[-a,b,c] for (a,b,c) in element[0] for element in velpos])
-#pos1,pos2 = C - pos2pixel(pos1,resizing), C - pos2pixel(pos2,resizing) #pickle's 0 is in middle of colony
-
+print("Done calculating velocities")
 '''NOT NEEDED'''
 for i in range(len(velpos)):
     velplotter(imgs[i],velpos[i,1],velpos[i,2],velpos[i,0],i)
+
+def sumtogrid(posit,vels,grid,gxs,gys):
+    x,y = posit[0],posit[1]
+    xchk,ychk = x/gxs, y/gys
+    grid[xchk,ychk] += np.array([vels[0],vels[1]])
+    return grid
     
+    
+gridfac = 64
+gx,gy = int(np.floor(imagesize/gridfac))-1,int(np.floor(imagesize/gridfac))-1
+print "Grid dimensions: ",gx,gy
+dgx,dgy = gridfac,gridfac
+pos = np.zeros((nframes-1,gx,gy,2))
+for step in range(nframes-1):
+    poscheck1 = (velpos[step,1]).astype(int)
+    vel2add = velpos[step,0]
+    for i in range(len(poscheck1)):
+        pos[step] = sumtogrid(poscheck1[i],vel2add[i],pos[step],dgx,dgy)
+#pos is now a grid of nframes,gx,gy,sumofvelocities
+           
+d = 165
 
-'''
-# Grid dimensions and spacing for regions of interest
-    gx,gy = int(np.floor(w/64))-1,int(np.floor(h/64))-1
-    dgx,dgy = 64,64
-    gside = 64
-
-    print "Image dimensions: ",w,h
-    print "Grid dimensions: ",gx,gy
-
-    print "Image intensity range:"
-    print np.max(im1), np.min(im1)
-    print np.max(im2), np.min(im2)
+for x in range(len(pos[8])):
+    for y in range(len(pos[8])):
+        #plt.quiver((gx*x)+C-d+25,C-(gy*y)+d-5,pos[1,x,y,0],-pos[1,x,y,1])
+        plt.quiver((dgx*x)+46.5,(dgy*y)+46.5,pos[1,x,y,0],pos[1,x,y,1])
 
 
-    # Filter images to remove noise
-    from scipy.ndimage.filters import gaussian_filter
-    im1 = [gaussian_filter(im1[i],1) for i in range(nframes)]
-    im2 = [gaussian_filter(im2[i],1) for i in range(nframes)]
-'''
+plt.show()
