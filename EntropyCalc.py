67287a827d08c8073eaebaf83fd120d9bab2434e
Ignacio Medina <inmedina@uc.cl>
2018-06-15 12:20:51 -0400

    Classes, Rework plots, Entropy calculations

---
+++ EntropyCalc.py
@@ -0,0 +1,78 @@
+#importa grid de PGE
+#histograma de velocidades de cada gridcell para todos los t
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import matplotlib.mlab as mlab
+
+def mainEC(grid,AV):
+    times = []
+    for t in range(len(grid.frames)):
+        variable = []
+        for ix in range(grid.gx):
+            for iy in range(grid.gy):
+                
+                if AV == 'vel':
+                    AV = ['vx','vy']
+                    vx = getattr(grid[t,ix,iy],AV[0])
+                    vy = getattr(grid[t,ix,iy],AV[1])
+                    elem = np.sqrt(((vx)**2)+(vy)**2)
+                    
+                if AV == 'x':
+                    aux = []
+                    for id in grid[t,ix,iy].cells.keys():
+                        var = getattr(grid[t,ix,iy].cells[id],'x')
+                        aux.append(var)
+                    elem = np.ravel(np.array(aux))
+                
+                variable.append(elem)
+            times.append(variable)     
+    return times
+
+
+def alltosingle(variab):
+    variab = np.array(variab)
+    variab = np.ravel(variab)
+    return variab
+
+def makehist(allvars,nbins,skip):
+    plt.hist(allvars[allvars >=skip],bins = nbins,normed = 1,edgecolor='#E6E6E6', color='#EE6666')
+             
+    
+def coolhist():
+    
+    #ax = plt.axes(axisbg='#E6E6E6')
+    ax = plt.axes()
+    ax.set_axisbelow(True)
+    plt.grid(color='w', linestyle='solid')
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+    ax.xaxis.tick_bottom()
+    ax.yaxis.tick_left()
+    ax.tick_params(colors='gray', direction='out')
+    for tick in ax.get_xticklabels():
+        tick.set_color('gray')
+    for tick in ax.get_yticklabels():
+        tick.set_color('gray')
+        
+def fitcurve(allvars,nbins,skip):
+    (mu, sigma) = norm.fit(allvars[allvars >=skip])
+    n, bins, patches =  plt.hist(allvars[allvars >=skip],bins = nbins,normed = 1,edgecolor='#E6E6E6', color='#EE6666')
+    y = mlab.normpdf( bins, mu, sigma)
+    plt.plot(bins, y, 'r--', linewidth=2)
+    plt.show()
+    
+alltimes = mainEC(grid,'vel')
+allvars = alltosingle(alltimes)
+#coolhist()
+fitcurve(allvars,15,0.05)
+makehist(allvars,15,0.05)
+'''
+for i in range(len(alltimes)):
+    plt.clf()
+    plt.hold(True)
+    fitcurve(alltimes[i],15,0.05)
+    plt.show()
+    plt.hold(False)
+    plt.pause(0.5)
+'''
--- PickleGridEntropy.py
+++ PickleGridEntropy.py
@@ -1,41 +1,93 @@
-import PickleVelocity as PV
+import cPickle
 import numpy as np
+import matplotlib.pyplot as plt
 
-def FullPickledx(cs1,cs2,dt,fact,center):
-    i = 0
-    cells_frame = np.array([])
-    ids1 = np.array([cell1.id for (id,cell1) in cs1.iteritems()])
-    ids2 = np.array([cell.id for (id,cell) in cs2.iteritems()])
-    for (id,cell) in cs1.iteritems():
-        try:
-            cs1[id].pos[0] = PV.pos2pixel(np.array(cell.pos),fact)[0]+center
-            cs1[id].pos[1] = -(PV.pos2pixel(np.array(cell.pos),fact)[1])+center
-            cs2[id].pos = cs2[id].pos #just to make it fail
-            cells_frame = np.append(cells_frame,cs1)
-        except KeyError:
-            #print 'Data2 has no id',id
-            i+=1
-    print 'Skipped', i,'ids, which should match this number:', len(ids2)-len(ids1)
-    return cells_frame
+class Grid():
+    def __init__(self,nframes,gx,gy,dgx,dgy,center,resizing):
+        self.grid = []
+        self.nframes = nframes
+        self.frames = np.arange(0,nframes,1)
+        self.gx = gx
+        self.gy = gy
+        self.dgx = dgx
+        self.dgy = dgy
+        self.center = center
+        self.resize = resizing
+        for frame in range(nframes):
+            frame1 = []
+            for ix in range(gx):
+                for iy in range(gy):
+                    frame1.append(Ensemble(frame,ix,iy,ix*dgx,iy*dgy))
+            self.grid.append(frame1)
+            
+    def __getitem__(self,i):
+        if type(i) == int:
+            return self.grid[i]
+        if len(i) == 3:
+            frame,ix,iy = i
+            return self.grid[frame][ix*self.gx+iy]
+      
+        
+class Ensemble(Grid):
+    def __init__(self,frame,ix,iy,px0,py0):
+        self.px = px0
+        self.py = py0
+        self.vx = 0
+        self.vy = 0
+        self.ix = ix
+        self.iy = iy
+        self.t1 = frame
+        self.cells = {}
+        self.skipped = 0
+        self.actualcells = 0
+    def addCell(self,cell,id): #cell = cellstate
+        self.cells[id] = cell
+    def CalcVel(self,nextstepcells,factor,dt):
+        dx,dy = 0,0
+        for id in self.cells.keys():
+            dx_cell = 0
+            dy_cell = 0
+            try:
+                dx_cell = nextstepcells[id].pos[0]-self.cells[id].pos[0]
+                dy_cell = nextstepcells[id].pos[1]-self.cells[id].pos[1]
+                self.actualcells +=1
+            except KeyError:
+                self.skipped +=1
+            dx += dx_cell
+            dy += dy_cell
+            
+        if self.actualcells != 0:
+            dx = dx/self.actualcells
+            dy = dy/self.actualcells
+            
+        self.vx = factor*dx/dt
+        self.vy = -factor*dy/dt
 
-def Histogram(grid,Index,nbins):
-    np.histogram(np.ravel(grid[Index]),bins = nbins)
-    
+def fname2pickle(fname):
+    if fname.endswith(".png") or fname.endswith(".jpg"):
+        newfname = fname[:len(fname)-4]+".pickle"
+    elif fname.endswith(".tiff"):
+        newfname = fname[:len(fname)-5]+".pickle"
+    return newfname
+
+def pos2pixel(ps,factr):
+    return factr*ps
+
+def checkcellingrid(cellstate,gsq,resize,dgx,dgy,center):
+    x,y = pos2pixel(cellstate.pos[0],resize)+center,-pos2pixel(cellstate.pos[1],resize)+center
+    xg,yg = gsq.px,gsq.py
+    if x >= xg and x < xg+dgx and y >= yg and y < yg+dgy:
+        return True
+    else:
+        return False
+'''    
 def ROI_velpos_cell(Method,gridcell,):
     gridcell = []
     if Method == 1:
         ak = 0
     #poner aca valores velocidad y posicion DE CADA celula en una grilla de grid
     #luego lo transformas a histograma2d   ak = 0
-def datacheck(neogrid,cell,gx,gy,dgx,dgy):
-    x = cell.pos[0]
-    y = cell.pos[1]
-    for ix in range(gx):
-        for iy in range(gy):
-            dgridx, dgridy = neogrid[ix,iy,1], neogrid[ix,iy,2]
-            if x >= dgridx and y >= dgridy and x <= dgridx+dgx and y <= dgridy+dgy:
-                return ix,iy
-            
+    
 def frame2grid(neogrid,dataframe,gx,gy,dgx,dgy,counter): 
     for cell in dataframe:
         try:
@@ -44,35 +96,77 @@ def frame2grid(neogrid,dataframe,gx,gy,dgx,dgy,counter):
         except TypeError:
             counter += 1
         return neogrid,counter
-        
+'''       
 def main(fname,startframe,nframes,dt,gridfac,worldsize = 250.0, forwards = True, GridMethod = None):
-    grid, gridstuff,velpos,imgs,data,csa = PV.main(fname, startframe, nframes, dt, gridfac, forwards=forwards, GridMethod=GridMethod, PGE = True)
-    gx,gy,dgx,dgy = gridstuff[0],gridstuff[1],gridstuff[2],gridstuff[3]
-    neogrid = np.zeros((nframes,gx,gy,4))
     
-    for frame in range(len(neogrid)):
-        for ix in range(gx):
-            for iy in range(gy):
-                neogrid[frame,ix,iy,:] = [grid[frame,ix,iy,2],grid[frame,ix,iy,3],grid[frame,ix,iy,4],0] #only keep position of grid and number of cells in grid
-                aux = []
-                neogrid[frame,ix,iy,3] = aux
+    fname2 = fname2pickle(fname)    
+    if forwards == True:
+        data = np.array([cPickle.load(open(fname2%(startframe+(i*dt)))) for i in range(nframes)]) #forward
+        imgs = np.array([plt.imread(open(fname%(startframe+(i*dt)))) for i in range(nframes)]) #forward
+
+    elif forwards == False:
+        data = np.array([cPickle.load(open(fname2%(startframe+(nframes-i)*dt))) for i in range(nframes)]) #backwards
+        imgs = np.array([plt.imread(open(fname%(startframe+(nframes-i)*dt))) for i in range(nframes)]) #backwards
+     
+    csa = np.array([element['cellStates'] for element in data])
+    
     imagesize = imgs[0].shape[0]
     resizing= imagesize/worldsize 
-    C = imagesize/2     
+    C = imagesize/2 
     
-    dataframes = np.array([FullPickledx(csa[i],csa[i+1],dt,resizing,C) for i in range(nframes-1)]) #dim (nframes-1,3) with dx,x1,x2
     
-    for frame in dataframes:
-        neogrid[frame],counter = frame2grid(neogrid[frame],dataframes,gx,gy,dgx,dgy)
-        print "There are",counter,"cells in the gaps between grid cells in step",frame
+    print "worldsize = ", worldsize
+    print "imagesize = ", imagesize
+    print "resizing factor = ", resizing  
+    gx,gy = int(np.floor(imagesize/gridfac)),int(np.floor(imagesize/gridfac)) #grid dimensions
+    print "Grid dimensions: ",gx,gy
+    dgx,dgy = gridfac,gridfac
+    
+    grid = Grid(nframes,gx,gy,dgx,dgy,C,resizing)
 
-    return neogrid
+    for t in range(nframes-1):
+        #Add cells to gridcells :
+        for ix in range(gx):
+            for iy in range(gy):
+                for (id,cell) in csa[t].iteritems():
+                    if checkcellingrid(cell,grid[t,ix,iy],resizing,dgx,dgy,grid.center) == True:
+                        grid[t,ix,iy].addCell(cell,id)
+        #calc velocity of grid at t = 0 to t+1:
+        skip,cellno,actual = 0,0,0
+        for ix in range(gx):
+            for iy in range(gy):
+                grid[t,ix,iy].CalcVel(csa[t+1],resizing,dt) 
+                skip,cellno,actual = skip+grid[t,ix,iy].skipped,cellno+len(grid[t,ix,iy].cells),actual+grid[t,ix,iy].actualcells
+                #Should we remove skipped cells? might affect entropy flux calculation  
+                #Move grids t+1:
+                grid[t+1,ix,iy].px = grid[t,ix,iy].px + grid[t,ix,iy].vx*dt
+                grid[t+1,ix,iy].py = grid[t,ix,iy].py + grid[t,ix,iy].vy*dt
+        print 'Skipped', skip,'ids, which should match this number:', cellno-actual
+     
+        
+    return grid,csa,imgs
 
+def squareplot(x,y,dgx,dgy):
+    plt.plot([x,x+dgx,x+dgx,x,x],[y,y,y+dgy,y+dgy,y],"k-",linewidth = 0.5)
+def plotensemble(grid,t,gx,gy,dgx,dgy):
+    for ix in range(gx):
+        for iy in range(gy):
+            if grid[t,ix,iy].actualcells != 0:
+                squareplot(grid[t,ix,iy].px,grid[t,ix,iy].py,dgx,dgy)
+                
+def plott(grid,imgs,gx,gy,dgx,dgy):
+    for i in range(len(grid.frames)-1):
+        plt.clf()
+        plt.hold(True)
+        plt.imshow(imgs[i],origin = 'lower')
+        plotensemble(grid,i,gx,gy,dgx,dgy)
+        plt.hold(False)
+        plt.pause(2)
+       
 
-
-afname = "/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-%04d0.png"
-astartframe = 40
-anframes = 5
-adt = 1 #There's a bit of trouble with this
+afname = "/Users/Ignacio/cellmodeller/data/Tutorial_1a-18-04-10-17-59/step-%05d.png"
+astartframe = 200
+anframes = 44
+adt = 10 #There's a bit of trouble with this
 agridfactor = 32 #pixels per grid
-aneogrid = main(afname,astartframe,anframes,adt,agridfactor,forwards = True, GridMethod = 1)
+grid,cs,ims = main(afname,astartframe,anframes,adt,agridfactor,forwards = False, GridMethod = 1)
