d0a15449c728d51995e4b0fc948511f393970451
Tim Rudge <timrudge@gmail.com>
2023-05-09 13:53:30 +0100

    Fit exponential velocity profile

--- scripts/fit_velocity_profile.py
+++ scripts/fit_velocity_profile.py
@@ -13,12 +13,18 @@ np.save('mean_growth_rate.npy', mean_growth_rate)
 
 # Compute colony edge velocity
 radius = np.load('radius.npy')
-vfront = savgol_filter(radius, 11, 3, deriv=1)
+vfront = savgol_filter(radius, 5, 3, deriv=1)
 np.save('vfront.npy', vfront)
 
+# Normalize velocity by edge vel
+vmag = np.load('vmag.npy')
+nt,_,_ = vmag.shape
+nvmag = vmag
+for frame in range(nt):
+    nvmag[frame,:,:] = vmag[frame,:,:] / vfront[frame]
+
 # Fit an exponential decay model to the velocity data
-def residual_func(edt, vmag, vfront):
-    nt,_,_ = edt.shape
+def residual_func(edt, nvmag, nt):
     def residuals(x):
         r0 = np.exp(x[0])
         C = x[1]
@@ -26,18 +32,37 @@ def residual_func(edt, vmag, vfront):
         for frame in range(nt):
             for ix in range(32):
                 for iy in range(32):
-                    if not np.isnan(vmag[frame,ix,iy]):
+                    if not np.isnan(nvmag[frame,ix,iy]):
                         r = edt[frame, ix*32:ix*32+64, iy*32:iy*32+64]
-                        model_vmag = np.exp(-r/r0)
+                        model_vmag = np.exp(-r/r0) + C
                         mean_model_vmag = model_vmag.mean()
-                        res.append(mean_model_vmag - vmag[frame, ix, iy]/vfront[frame])
+                        res.append(mean_model_vmag - nvmag[frame, ix, iy])
         return res
     return residuals
 
 edt = np.load('edt.npy')
-vmag = np.load('vmag.npy')
-res = least_squares(residual_func(edt, vmag, vfront), x0=(np.log(50),0))
+res = least_squares(residual_func(edt, nvmag, nt), x0=(np.log(50),0))
 r0 = np.exp(res.x[0])
 C = res.x[1]
 
 print(f'r0 = {r0}, C = {C}')
+
+# Make a plot to see how good the fit is
+radpos = np.load('radpos.npy')
+x = radpos[~np.isnan(vmag)].ravel()
+y = np.zeros((0,))
+for frame in range(nt):
+    for ix in range(32):
+        for iy in range(32):
+            if not np.isnan(nvmag[frame,ix,iy]):
+                r = edt[frame, ix*32:ix*32+64, iy*32:iy*32+64]
+                model_vmag = np.exp(-r/r0) + C
+                mean_model_vmag = model_vmag.mean()
+                y = np.append(y, mean_model_vmag)
+
+plt.plot(x, nvmag[~np.isnan(nvmag)], 'x')
+plt.plot(x, y, '.')
+plt.show()
+
+
+
