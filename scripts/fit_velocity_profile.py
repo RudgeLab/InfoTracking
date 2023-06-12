5b6ca3fe9ea026273704ceedc328de07e1526e63
Tim Rudge <timrudge@gmail.com>
2023-06-12 13:55:04 +0100

    Playing with analysis scripts, parameters etc.

--- scripts/fit_velocity_profile.py
+++ scripts/fit_velocity_profile.py
@@ -5,23 +5,24 @@ from scipy.optimize import least_squares
 import matplotlib.pyplot as plt
 
 # Compute average growth rate of colony at each time point
-area = np.load('area.npy')
-sarea = savgol_filter(area, 11, 3)
-dsarea = savgol_filter(area, 11, 3, deriv=1)
-mean_growth_rate = dsarea / area
-np.save('mean_growth_rate.npy', mean_growth_rate)
+#area = np.load('area.npy')
+#sarea = savgol_filter(area, 5, 3)
+#dsarea = savgol_filter(area, 5, 3, deriv=1)
+#mean_growth_rate = dsarea / area
+#np.save('mean_growth_rate.npy', mean_growth_rate)
 
 # Compute colony edge velocity
-radius = np.load('radius.npy')
-vfront = savgol_filter(radius, 5, 3, deriv=1)
-np.save('vfront.npy', vfront)
+#radius = np.load('radius.npy')
+#vfront = savgol_filter(radius, 5, 3, deriv=1)
+#np.save('vfront.npy', vfront)
+vfront = 6.585722451885091 
 
 # Normalize velocity by edge vel
 vmag = np.load('vmag.npy')
 nt,_,_ = vmag.shape
 nvmag = vmag
 for frame in range(nt):
-    nvmag[frame,:,:] = vmag[frame,:,:] / vfront[frame]
+    nvmag[frame,:,:] = vmag[frame,:,:] / vfront #[frame]
 
 # Fit an exponential decay model to the velocity data
 def residual_func(edt, nvmag, nt):
@@ -30,10 +31,10 @@ def residual_func(edt, nvmag, nt):
         C = x[1]
         res = []
         for frame in range(nt):
-            for ix in range(32):
-                for iy in range(32):
+            for ix in range(64):
+                for iy in range(64):
                     if not np.isnan(nvmag[frame,ix,iy]):
-                        r = edt[frame, ix*32:ix*32+64, iy*32:iy*32+64]
+                        r = edt[frame, ix*16:ix*16+64, iy*16:iy*16+64]
                         model_vmag = np.exp(-r/r0) + C
                         mean_model_vmag = model_vmag.mean()
                         res.append(mean_model_vmag - nvmag[frame, ix, iy])
@@ -41,7 +42,7 @@ def residual_func(edt, nvmag, nt):
     return residuals
 
 edt = np.load('edt.npy')
-res = least_squares(residual_func(edt, nvmag, nt), x0=(np.log(50),0))
+res = least_squares(residual_func(edt, nvmag, nt), x0=(np.log(20),0))
 r0 = np.exp(res.x[0])
 C = res.x[1]
 
@@ -52,17 +53,21 @@ radpos = np.load('radpos.npy')
 x = radpos[~np.isnan(vmag)].ravel()
 y = np.zeros((0,))
 for frame in range(nt):
-    for ix in range(32):
-        for iy in range(32):
+    for ix in range(64):
+        for iy in range(64):
             if not np.isnan(nvmag[frame,ix,iy]):
-                r = edt[frame, ix*32:ix*32+64, iy*32:iy*32+64]
+                r = edt[frame, ix*16:ix*16+64, iy*16:iy*16+64]
                 model_vmag = np.exp(-r/r0) + C
                 mean_model_vmag = model_vmag.mean()
                 y = np.append(y, mean_model_vmag)
 
-plt.plot(x, nvmag[~np.isnan(nvmag)], 'x')
+plt.plot(x, nvmag[~np.isnan(nvmag)], '.', alpha=0.2)
 plt.plot(x, y, '.')
 plt.show()
+
+# The edge growth rate = 2 * edge velocity / r0
+mu0 = 2 * vfront / r0
+np.save('mu0.npy', mu0)
 
 
 
